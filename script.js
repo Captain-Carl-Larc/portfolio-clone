2c3a4ee5463c714334e95a15b9061806419560f3
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-05-14 07:48:51 +0300

    hero section made

---
+++ script.js
@@ -0,0 +1,5 @@
+// Hero Section Animations
+window.addEventListener('DOMContentLoaded', () => {
+  const heroSection = document.querySelector('.hero-section');
+  heroSection.classList.add('animate');
+});
