1d6cfa93d0ce541e44f34ac3615db07623b962e6
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-05-16 00:12:54 +0300

    navbar made

--- script.js
+++
@@ -1,5 +0,0 @@
-// Hero Section Animations
-window.addEventListener('DOMContentLoaded', () => {
-  const heroSection = document.querySelector('.hero-section');
-  heroSection.classList.add('animate');
-});
